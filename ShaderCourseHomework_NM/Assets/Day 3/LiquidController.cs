cf2c9ec67e3552037138db5f698437d03da51993
NamgarM <50959596+NamgarM@users.noreply.github.com>
2021-03-03 19:48:52 +0100

    Day 3: liquid(done) and hourglasses(not done)

---
+++ ShaderCourseHomework_NM/Assets/Day 3/LiquidController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidController : MonoBehaviour
+{
+    public Material mat;
+    public float liquidAmount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("_LiquidAmount", liquidAmount);
+        //mat.SetVector("_LiquidLevelNormal")
+    }
+}
---
+++ ShaderCourseHomework_NM/Assets/Day 3/LiquidShaderDriver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class LiquidShaderDriver : MonoBehaviour
+{
+    public Material upperMat;
+    public Material downMat;
+    public float upperLiquidAmount = 0;
+    public float downLiquidAmount = 0;
+
+    // Update is called once per frame
+    void Update()
+    { 
+        upperMat.SetFloat("_LiquidAmount", upperLiquidAmount);
+        downMat.SetFloat("_LiquidAmount", downLiquidAmount);
+    }
+}
